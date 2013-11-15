267a2899193ab7b2b5b95d92374dea50ea9f2031
Mannen mannensson <victor_axelsson@hotmail.com>
2013-11-15 15:01:27 +0100

    scriptatttttt

---
+++ Script_kiddie.bat
@@ -0,0 +1,2 @@
+@echo off
+start %comspec% /c "mode 40,10&title My Popup&color 1e&echo.&echo. Today is %date%&echo.&echo. Press a key!&pause>NUL"
--- temp.bat
+++
@@ -1,5 +0,0 @@
-Set objArgs = WScript.Arguments
-messageText = objArgs(0)
-MsgBox messageText
-
-cscript MessageBox.vbs "This will be shown in a popup."
